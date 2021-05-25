edac2b3552a3452efbfd9139a6670370427e05d4
farzhana <farzhana.pachu@gmail.com>
2021-05-25 15:44:41 +0000

    commit

---
+++ src/OopsConcepts/Typecasting.java
@@ -0,0 +1,24 @@
+package OopsConcepts;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//-----implicit typecasting-------
+		
+		char a = 'a';
+		System.out.println("valuve = " + a);
+		
+		int b = 'b';
+		System.out.println("value = " + b);
+		
+		//-Explicit typecasting--------
+		
+		double x = 45.5;
+		float y = (float) x;
+		
+		System.out.println("value = " + y);
+	}
+
+}
